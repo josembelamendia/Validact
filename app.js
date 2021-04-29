4f374cdb2e1d803a35d03398727bcf196b139aa7
Jose <josembelamendia@gmail.com>
2021-04-29 19:38:48 -0300

    Validation rules in config.json

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 (function() {
 
+var VALIDATION_RULES = [];
+var CUSTOM_INPUTS = [];
 const FORM = document.getElementById('validable-form');
 const FORM_INPUTS = Array.from(document.getElementsByClassName('validable'));
 const ERROR_BORDER = "1px solid red"
@@ -7,7 +9,14 @@ const SUCCESS_BORDER = "1px solid green"
 const NO_BORDER = "none"
 const RULES = ["min", "max", "uppercase"];
 
-var CUSTOM_INPUTS = [];
+$.ajax({
+    url: 'config.json',
+    success: function(data) {
+        VALIDATION_RULES = data;
+
+        init();
+    }
+})
 
 class CustomInput {
 
@@ -52,6 +61,12 @@ class CustomInput {
             }
             case "min": {
                 return this.input.value.trim().length >= ruleValue;
+            }
+            case "uppercase": {
+                if(!ruleValue) {
+                    return !/[A-Z]/.test(this.input.value.trim())
+                }
+                return /[A-Z]/.test(this.input.value.trim())
             }
         }
     }
@@ -99,6 +114,12 @@ function getErrorForRule(rule, value) {
         case "max": {
             return `Máximo de caracteres permitido: ${value}`;
         }
+        case "uppercase": {
+            if(value) {
+                return `Es necesario que la palabra contenga una mayúscula.`
+            }
+            return `La palabra no puede contener una mayúscula.`
+        }
     }
 }
 
@@ -130,71 +151,53 @@ function init() {
         
         CUSTOM_INPUTS.push(input);
     })
-}
 
-const VALIDATION_RULES = {
-    name: {
-        min: 1,
-        max: 16
-    },
-    lastname: {
-        min: 1
-    },
-    password: {
-        min: 8,
-        max: 16,
-    },
-    dni: {
-        min: 8,
-        max: 8
-    },
-}
+    FORM.addEventListener('submit', event => {
 
-init()
+        CUSTOM_INPUTS.forEach(customInput => {
+    
+            if(!customInput.isValidated()) {
+                if(!customInput.animation) {
+                    if(!customInput.isBorderToggled()) {
+                        customInput.input.style.border = ERROR_BORDER
+                        
+                        setTimeout(() => {
 
-FORM.addEventListener('submit', event => {
-
-    CUSTOM_INPUTS.forEach(customInput => {
-
-        if(!customInput.isValidated()) {
-            if(!customInput.animation) {
-                if(!customInput.isBorderToggled()) {
-                    customInput.input.style.border = ERROR_BORDER
-                    setTimeout(() => {
-                        customInput.input.style.border = NO_BORDER;
-                        customInput.animation = false;
-                    }, 3000);
+                            customInput.input.style.border = NO_BORDER;
+                            customInput.getValidationMessageField().style.display = "none"
+                            customInput.animation = false;
+                        }, 3000);
+                    }
                 }
+    
+                customInput.animation = true;
+                event.preventDefault()
             }
-
-            customInput.animation = true;
-            event.preventDefault()
-        }
+        })
+    
     })
-
-})
-
-CUSTOM_INPUTS.forEach(customInput => {
-    customInput.input.addEventListener("keyup", event => {
-        customInput.setBorderToggled(true)
-
-        if(!customInput.isValidated()) {
-            customInput.input.style.border = ERROR_BORDER
-            return;
-        }
-        customInput.input.style.border = SUCCESS_BORDER
+    
+    CUSTOM_INPUTS.forEach(customInput => {
+        customInput.input.addEventListener("keyup", event => {
+            customInput.setBorderToggled(true)
+    
+            if(!customInput.isValidated()) {
+                customInput.input.style.border = ERROR_BORDER
+                return;
+            }
+            customInput.input.style.border = SUCCESS_BORDER
+        })
+    
+        customInput.input.addEventListener("click", event => {
+            customInput.setBorderToggled(true)
+    
+            if(!customInput.isValidated()) {
+                customInput.input.style.border = ERROR_BORDER
+                return;
+            }
+            customInput.input.style.border = SUCCESS_BORDER
+        })
     })
-
-    customInput.input.addEventListener("click", event => {
-        customInput.setBorderToggled(true)
-
-        if(!customInput.isValidated()) {
-            customInput.input.style.border = ERROR_BORDER
-            return;
-        }
-        customInput.input.style.border = SUCCESS_BORDER
-    })
-})
-
+}
 
 })();
